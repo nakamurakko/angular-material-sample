fb39e23e4edf30a5e1c864b87e67188d46b5672b
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2022-01-12 16:29:57 +0900

    AppComponent を整理。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
